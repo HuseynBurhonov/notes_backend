31db255daf97258018bbb1ca168121fb397f2754
Huseyn <77714940+HuseynBurhonov@users.noreply.github.com>
2022-05-04 15:55:55 +0500

    Planned project structure

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Notes.Domain
+{
+    internal class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
