fc178a1227c87140da9a4ffc390ec56f810dc54d
Huseyn <77714940+HuseynBurhonov@users.noreply.github.com>
2022-05-04 17:21:32 +0500

    Added and configured automapper

---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    internal interface IMapWith<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
